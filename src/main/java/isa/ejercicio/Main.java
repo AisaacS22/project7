f8557d3a9841a014d4a5e03433412d1abf11b0b3
isaacflores2205@gmail.com <isaacflores2205@gmail.com>
2024-04-06 08:58:29 -0600

    Clase 7

---
+++ src/main/java/isa/ejercicio/Main.java
@@ -0,0 +1,11 @@
+package isa.ejercicio;
+
+import Listas.Lista;
+
+public class Main {
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+        lista.crearLista().visualizar();
+    }
+}
+
